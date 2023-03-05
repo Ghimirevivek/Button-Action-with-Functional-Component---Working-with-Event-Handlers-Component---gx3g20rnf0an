e263005b52cec738033896d0be0b12eba5d89fc5
Ghimirevivek <108508449+Ghimirevivek@users.noreply.github.com>
2023-03-05 22:22:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const paragraph = ()=>{
+    return(
+      <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+    )
+  }
   return (
     <div id="main">
       // Do not alter the main div
+    <button onClick={paragraph}>Click</button
     </div>
   );
 }
