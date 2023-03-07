9e393f153cb77a6a1468587da92096fe42afa66f
Ghimirevivek <108508449+Ghimirevivek@users.noreply.github.com>
2023-03-07 17:13:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
-import {useState} from "react"
 function App() {
   const [initial,setInitial] = useState(false)
   return (
